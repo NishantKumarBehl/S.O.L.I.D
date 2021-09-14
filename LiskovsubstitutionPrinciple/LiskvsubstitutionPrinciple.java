83a1f321d830e2523116a64a114740ade21dce37
Nishant Kumar Behl <Nishant.Behl@oneadvanced.com>
2021-09-15 05:27:42 +0530

    added liskov file

---
+++ LiskovsubstitutionPrinciple/LiskvsubstitutionPrinciple.java
@@ -0,0 +1,10 @@
+package LiskovsubstitutionPrinciple;
+
+// Principle states that the subtype should be able to replace or substitute
+// the behaviour of the class, it is subtype of
+
+
+public class LiskvsubstitutionPrinciple {
+
+    
+}
