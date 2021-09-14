f285683a74e7be8ddf9349af07199650c606623e
Nishant Kumar Behl <Nishant.Behl@oneadvanced.com>
2021-09-14 07:27:06 +0530

    Added classes and factory

--- OpenClosedPrincipleWithStrategyPattern/OpenClosedP.java
+++ OpenClosedPrincipleWithStrategyPattern/OpenClosedP.java
@@ -1,5 +1,7 @@
 package OpenClosedPrincipleWithStrategyPattern;
 
+import javax.management.RuntimeErrorException;
+
 public class OpenClosedP {
     // Open for Extension and Closed for modification
     // you do this by abstracting an interface that can be passed as parameter
@@ -20,13 +22,79 @@ public class OpenClosedP {
     // are in violcation of OCP. so lets see how do you hand it step by Step
 
     // Step 1: lets have a class Bank account that has a method money transfer
-    // Step 2: Create another class that implements Inlt. money tranfer feature
-    // Step 3: 
+    // Step 2: Create another class for Intl Bank account and implements Inlt. money tranfer feature 
+    //         which means we now need to support two different types of transfer
+    // Step 3: Abstract the money transfer functionality into an interface as
+    //         the implementation of international and local tranfers will differ
+    //         abstracting method into interface will remove the need to modify the 
+    //         BaseClass BankAccount to support international transfer while adding
+    //         International bank account feature
+    // Step 4: 
+    public enum TransferType {LOCAL, INTL};
 
     public static void main(String[] args) {
 
+         TransferType type = TransferType.INTL;
+         double amount = 1000000;
+        
+         MoneyTransferI transfer = MoneyTranferProcessorFactory.build(type);
+
          
 
+         processMoneyTransfer(amount,  type);
+
+
     }
+
+    // Both BankAccount and Intl Bank Account
+    public class BankAccount implements MoneyTransferI{
+
+            public void transferMoney(double amount){
+
+                
+                System.out.println("LOCAL Transfer Your money is successfully transferred");
+            }
+    }
+
+    public class IntlBankAccount implements MoneyTransferI{
+
+        public void transferMoney(double amount){
+            System.out.println("INTL Transfer Your money is successfully transferred");
+        }
+
+}
+
+    //Abstracting the money transfer method\functionality into a Interface-STRATEGY
+    public interface  MoneyTransferI{
+
+        public void transferMoney(double amount);
+
+    }
+
+    public static class MoneyTranferProcessorFactory {
+
+        public static MoneyTransferI build(TransferType type){
+
+            if (type == TransferType.INTL){
+                return new IntlBankAccount();
+            } 
+            
+            else if(type == TransferType.LOCAL){
+                return new BankAccount();
+            
+            throw new RuntimeException("Please provide the valid Transfer Type")
+
+            }
+        }
+
+        public void processMoneyTransfer(double amount, TransferType type){
+
+            MoneyTranferProcessorFactory factory = new MoneyTranferProcessorFactory();
+            MoneyTransferI mtt = factory.build(type);
+            mtt.transferMoney(amount);
+        }
+    }
+
+
 
 }
