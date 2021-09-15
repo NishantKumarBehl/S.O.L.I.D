e302d53bad6a664ba428d4311f7aa12e88e41233
Nishant Kumar Behl <Nishant.Behl@oneadvanced.com>
2021-09-15 06:33:10 +0530

    Added violations

--- LiskovsubstitutionPrinciple/LiskvsubstitutionPrinciple.java
+++ LiskovsubstitutionPrinciple/LiskvsubstitutionPrinciple.java
@@ -3,6 +3,14 @@ package LiskovsubstitutionPrinciple;
 // Principle states that the subtype should be able to replace or substitute
 // the behaviour of the class, it is subtype of
 
+// Violations - How do you spot
+// #1 Empty methods or Partially implemented interfaces that are not implementable in class that is extending base class
+// e.g @Override public void fly(int alt){ throw exception or do nothing}
+// #2 Harden preconditions
+// e.g. square extending rectangle. square doesnt need width but just height
+//      Rectangle r = new sqaure() , r.setWidth(10), r. setHeight(20),r.area() will give wrong answer
+// #3 Type checking of class to implement certain behavior with forceful typecasting
+
 
 public class LiskvsubstitutionPrinciple {
 
