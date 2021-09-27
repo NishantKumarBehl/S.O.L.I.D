183dc8ee5129a513c3dc484c23616943991c6942
NishantKumarBehl <47054141+NishantKumarBehl@users.noreply.github.com>
2021-09-27 10:10:23 +0530

    Update Account.java

--- LiskovsubstitutionPrinciple/AbstractClass/Account.java
+++ LiskovsubstitutionPrinciple/AbstractClass/Account.java
@@ -1,5 +1,8 @@
 package LiskovsubstitutionPrinciple.AbstractClass;
 
+        // Design abstracted Account class to only handle deposit and the widrawl method
+        // shall be avaialbe in types of accounts(WidrawableAccount) that support widrawal along with deposit
+        // With this Widrawal service only interacts with Widrawable account
 
 public abstract class Account {
     public abstract double deposit(Double amount);
