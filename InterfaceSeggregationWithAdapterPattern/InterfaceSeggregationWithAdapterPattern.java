f7e7b90bc5dfb19e8ab37fa17cb6532e52c24bfe
Nishant Kumar Behl <Nishant.Behl@oneadvanced.com>
2021-09-28 05:46:16 +0530

    created separate files

---
+++ InterfaceSeggregationWithAdapterPattern/InterfaceSeggregationWithAdapterPattern.java
@@ -0,0 +1,20 @@
+package InterfaceSeggregationWithAdapterPattern;
+
+//Principle: Clients shold not be forced to depend on Interfaces that they do not use
+
+
+// ProblemStatement:  You have Interface Payment that supports status and initiative payment & getpayments
+// New feature LoanPayment is to be added, this needs Initiaterepayment & LoanSettlement as new method
+// Implment this using ISP
+
+
+public class InterfaceSeggregationWithAdapterPattern {
+ 
+    
+public static void main(String[] args) {
+    //put client implementation here
+}
+
+
+
+}
